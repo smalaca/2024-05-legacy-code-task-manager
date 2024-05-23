8ad59b374aa65747ca09df7a760abb03c63d0182
smalaca <ab13.krakow@gmail.com>
2024-05-23 13:31:09 +0200

    REFACT - remove controlling flow by exceptions in findById

--- src/main/java/com/smalaca/taskamanager/api/rest/TeamController.java
+++ src/main/java/com/smalaca/taskamanager/api/rest/TeamController.java
@@ -67,24 +67,30 @@ public class TeamController {
     @GetMapping("/{id}")
     @Transactional
     public ResponseEntity<TeamDto> findById(@PathVariable Long id) {
-        try {
-            Team team = getTeamById(id);
-            TeamDto dto = new TeamDto();
-            dto.setId(team.getId());
-            dto.setName(team.getName());
+        Optional<Team> team1 = teamRepository.findById(id);
 
-            if (team.getCodename() != null) {
-                dto.setCodenameShort(team.getCodename().getShortName());
-                dto.setCodenameFull(team.getCodename().getFullName());
-            }
-
-            dto.setDescription(team.getDescription());
-            dto.setUserIds(team.getMembers().stream().map(User::getId).collect(toList()));
-
-            return new ResponseEntity<>(dto, HttpStatus.OK);
-        } catch (TeamNotFoundException exception) {
+        if (doNotExist(team1)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
+        Team team = team1.get();
+        TeamDto dto = new TeamDto();
+        dto.setId(team.getId());
+        dto.setName(team.getName());
+
+        if (team.getCodename() != null) {
+            dto.setCodenameShort(team.getCodename().getShortName());
+            dto.setCodenameFull(team.getCodename().getFullName());
+        }
+
+        dto.setDescription(team.getDescription());
+        dto.setUserIds(team.getMembers().stream().map(User::getId).collect(toList()));
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    private boolean doNotExist(Optional<Team> team1) {
+        return team1.isEmpty();
     }
 
     @PostMapping
