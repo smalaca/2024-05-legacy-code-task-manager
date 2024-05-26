d508f9226c0985bd6e4bb3dbf86e673b593debb7
smalaca <ab13.krakow@gmail.com>
2024-05-26 14:07:58 +0200

    REFACT - remove usage of TaskRepository from command package

--- src/main/java/com/smalaca/taskmanager/command/task/TaskAddWatcherCommand.java
+++ src/main/java/com/smalaca/taskmanager/command/task/TaskAddWatcherCommand.java
@@ -1,9 +1,67 @@
 package com.smalaca.taskmanager.command.task;
 
-class TaskAddWatcherCommand {
-    private final TaskDomainModelRepository taskDomainModelRepository;
+import com.smalaca.taskamanager.dto.WatcherDto;
+import com.smalaca.taskamanager.exception.UserNotFoundException;
+import com.smalaca.taskamanager.model.embedded.EmailAddress;
+import com.smalaca.taskamanager.model.embedded.PhoneNumber;
+import com.smalaca.taskamanager.model.embedded.Watcher;
+import com.smalaca.taskamanager.model.entities.Task;
+import com.smalaca.taskamanager.model.entities.User;
+import com.smalaca.taskamanager.repository.UserRepository;
 
-    TaskAddWatcherCommand(TaskDomainModelRepository taskDomainModelRepository) {
-        this.taskDomainModelRepository = taskDomainModelRepository;
+import java.util.Optional;
+
+class TaskAddWatcherCommand {
+    private final TaskDomainModelRepository taskRepository;
+    private final UserRepository userRepository;
+
+    TaskAddWatcherCommand(TaskDomainModelRepository taskRepository, UserRepository userRepository) {
+        this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
+    }
+
+    void process(long id, WatcherDto dto) {
+        Task entity1 = findTaskBy(id);
+
+        User entity2 = findUserBy(dto.getId());
+        Watcher entity3 = new Watcher();
+        entity3.setFirstName(entity2.getUserName().getFirstName());
+        entity3.setLastName(entity2.getUserName().getLastName());
+
+        if (entity2.getEmailAddress() != null) {
+            EmailAddress entity4 = new EmailAddress();
+            entity4.setEmailAddress(entity2.getEmailAddress().getEmailAddress());
+            entity3.setEmailAddress(entity4);
+        }
+
+        if (entity2.getPhoneNumber() != null) {
+            PhoneNumber entity5 = new PhoneNumber();
+            entity5.setNumber(entity2.getPhoneNumber().getNumber());
+            entity5.setPrefix(entity2.getPhoneNumber().getPrefix());
+            entity3.setPhoneNumber(entity5);
+        }
+        entity1.addWatcher(entity3);
+
+        taskRepository.update(new TaskDomainModel(entity1));
+    }
+
+    private Task findTaskBy(long id) {
+        Optional<TaskDomainModel> found = taskRepository.findById(id);
+
+        if (found.isEmpty()) {
+            throw new TaskDomainModelDoesNotExistException(id);
+        }
+
+        return found.get().asTask();
+    }
+
+    private User findUserBy(Long id) {
+        Optional<User> found = userRepository.findById(id);
+
+        if (found.isEmpty()) {
+            throw new UserNotFoundException();
+        }
+
+        return found.get();
     }
 }
--- src/main/java/com/smalaca/taskmanager/command/task/TaskCommandApi.java
+++ src/main/java/com/smalaca/taskmanager/command/task/TaskCommandApi.java
@@ -1,22 +1,12 @@
 package com.smalaca.taskmanager.command.task;
 
 import com.smalaca.taskamanager.dto.WatcherDto;
-import com.smalaca.taskamanager.exception.UserNotFoundException;
-import com.smalaca.taskamanager.model.embedded.EmailAddress;
-import com.smalaca.taskamanager.model.embedded.PhoneNumber;
-import com.smalaca.taskamanager.model.embedded.Watcher;
-import com.smalaca.taskamanager.model.entities.Task;
-import com.smalaca.taskamanager.model.entities.User;
 import com.smalaca.taskamanager.repository.TaskRepository;
 import com.smalaca.taskamanager.repository.UserRepository;
 import com.smalaca.taskmanager.command.owner.OwnerDomainModelRepository;
 import com.smalaca.taskmanager.command.story.StoryDomainModelRepository;
 
-import java.util.Optional;
-
 public class TaskCommandApi {
-    private final UserRepository userRepository;
-    private final TaskRepository taskRepository;
     private final TaskCreateCommand taskCreateCommand;
     private final TaskUpdateCommand taskUpdateCommand;
     private final TaskDeleteCommand taskDeleteCommand;
@@ -26,12 +16,10 @@ public class TaskCommandApi {
             UserRepository userRepository, TaskRepository taskRepository,
             StatusChangeService statusChangeService, TaskDomainModelRepository taskDomainModelRepository,
             OwnerDomainModelRepository ownerDomainModelRepository, StoryDomainModelRepository storyDomainModelRepository) {
-        this.userRepository = userRepository;
-        this.taskRepository = taskRepository;
         taskCreateCommand = new TaskCreateCommand(taskDomainModelRepository, ownerDomainModelRepository, storyDomainModelRepository);
         taskUpdateCommand = new TaskUpdateCommand(taskDomainModelRepository, ownerDomainModelRepository, statusChangeService);
         taskDeleteCommand = new TaskDeleteCommand(taskDomainModelRepository);
-        taskAddWatcherCommand = new TaskAddWatcherCommand(taskDomainModelRepository);
+        taskAddWatcherCommand = new TaskAddWatcherCommand(taskDomainModelRepository, userRepository);
     }
 
     public Long create(CreateTaskDto createTaskDto) {
@@ -47,47 +35,6 @@ public class TaskCommandApi {
     }
 
     public void addWatcher(long id, WatcherDto dto) {
-        Task entity1 = findTaskBy(id);
-
-        User entity2 = findUserBy(dto.getId());
-        Watcher entity3 = new Watcher();
-        entity3.setFirstName(entity2.getUserName().getFirstName());
-        entity3.setLastName(entity2.getUserName().getLastName());
-
-        if (entity2.getEmailAddress() != null) {
-            EmailAddress entity4 = new EmailAddress();
-            entity4.setEmailAddress(entity2.getEmailAddress().getEmailAddress());
-            entity3.setEmailAddress(entity4);
-        }
-
-        if (entity2.getPhoneNumber() != null) {
-            PhoneNumber entity5 = new PhoneNumber();
-            entity5.setNumber(entity2.getPhoneNumber().getNumber());
-            entity5.setPrefix(entity2.getPhoneNumber().getPrefix());
-            entity3.setPhoneNumber(entity5);
-        }
-        entity1.addWatcher(entity3);
-
-        taskRepository.save(entity1);
-    }
-
-    private Task findTaskBy(long id) {
-        Optional<Task> found = taskRepository.findById(id);
-
-        if (found.isEmpty()) {
-            throw new TaskDomainModelDoesNotExistException(id);
-        }
-
-        return found.get();
-    }
-
-    private User findUserBy(Long id) {
-        Optional<User> found = userRepository.findById(id);
-
-        if (found.isEmpty()) {
-            throw new UserNotFoundException();
-        }
-
-        return found.get();
+        taskAddWatcherCommand.process(id, dto);
     }
 }
